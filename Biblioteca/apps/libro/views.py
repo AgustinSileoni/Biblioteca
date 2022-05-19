bbf8bc2382209f58130d908b5abd261c295d4ba1
AgustinSileoni <agustinsileoni98@gmail.com>
2022-05-19 12:09:00 -0300

    Plantillas creadas con herencia para un formulario que crea autores

--- Biblioteca/apps/libro/views.py
+++ Biblioteca/apps/libro/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from enum import auto
+from django.shortcuts import redirect, render
+
+from .forms import AutorForm
 
 # Create your views here.
+
+def Home(request):
+    return render(request, 'index.html')
+
+def crearAutor(request):
+    if request.method == 'POST':
+        autor_form = AutorForm(request.POST)
+        if autor_form.is_valid():   ##
+            autor_form.save()   ##Guarda los datos (Propio de form)
+            return redirect('index') ##Me redirige a una url
+    else:
+        autor_form = AutorForm()
+    return render(request, 'libro/crear_autor.html',{'autor_form':autor_form})
