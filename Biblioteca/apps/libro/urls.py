622fb5a8f3b6a02021de68ad837a21a13535a04f
AgustinSileoni <agustinsileoni98@gmail.com>
2022-05-21 15:38:34 -0300

    Agregada la vista para editar y eliminar registros

--- Biblioteca/apps/libro/urls.py
+++ Biblioteca/apps/libro/urls.py
@@ -1,8 +1,14 @@
+from unicodedata import name
 from django.urls import path
 
-from .views import crearAutor
+from .views import crearAutor, editarAutor, eliminarAutorDirecto, eliminarAutorDirecto, eliminarAutorLogico,listarAutor
 
 
 urlpatterns=[
-    path('crear_autor/', crearAutor, name = 'crear_autor')
+    path('crear_autor/', crearAutor, name = 'crear_autor'),
+    path('listar_autor/', listarAutor, name= 'listar_autor'),
+    path('editar_autor/<int:id>', editarAutor, name= 'editar_autor'),
+    path('eliminar_autor_directo/<int:id>',eliminarAutorDirecto, name='eliminar_autor_directo'),
+    path('eliminar_autor_logico/<int:id>',eliminarAutorLogico, name='eliminar_autor_logico'),
+    
 ]
--- Biblioteca/apps/libro/views.py
+++ Biblioteca/apps/libro/views.py
@@ -1,5 +1,7 @@
 from enum import auto
 from django.shortcuts import redirect, render
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Autor
 
 from .forms import AutorForm
 
@@ -9,11 +11,47 @@ def Home(request):
     return render(request, 'index.html')
 
 def crearAutor(request):
-    if request.method == 'POST':
-        autor_form = AutorForm(request.POST)
-        if autor_form.is_valid():   ##
+    if request.method == 'POST':  #Si el metodo utilizado es post
+        autor_form = AutorForm(request.POST)  ##Relleno autor_form con los datos del POST
+        if autor_form.is_valid():   ##Verifica que los datos ingresados sean validos (Propio de form)
             autor_form.save()   ##Guarda los datos (Propio de form)
             return redirect('index') ##Me redirige a una url
     else:
         autor_form = AutorForm()
     return render(request, 'libro/crear_autor.html',{'autor_form':autor_form})
+
+def listarAutor(request):
+    autores = Autor.objects.filter(estado = True)
+    return render(request, 'libro/listar_autor.html',{'autores':autores})
+	
+def editarAutor(request,id):
+    autor_form= None
+    error = None
+    try: 
+        autor = Autor.objects.get(id=id)
+        if request.method == 'GET':
+            autor_form = AutorForm(instance = autor)
+        else:
+            autor_form = AutorForm(request.POST, instance = autor)
+            if autor_form.is_valid():
+                    autor_form.save()
+            redirect('index')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request,'libro/crear_autor.html',{'autor_form':autor_form,'error':error})
+
+def eliminarAutorDirecto(request,id):
+	autor= Autor.objects.get(id=id)
+	if request.method == 'POST':
+		autor.delete()
+		return redirect('libro:listar_autor')	
+	return render (request, 'libro/eliminar_autor_directo.html',{'autor':autor})
+
+def eliminarAutorLogico(request,id):
+    autor = Autor.objects.get(id = id)
+    if request.method == 'POST':
+        autor.estado = False
+        autor.save()
+        return redirect('libro:listar_autor')	
+    return render (request, 'libro/eliminar_autor_logico.html',{'autor':autor})
